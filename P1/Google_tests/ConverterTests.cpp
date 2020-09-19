de8e8e983dadfe98249b47175c7c873412bbe360
UserNameIsAlredyTaken <gdaniltatarinof@gmail.com>
2020-09-19 14:04:11 +0300

    delete old

--- P1/Google_tests/ConverterTests.cpp
+++
@@ -1,8 +0,0 @@
-#include "gtest/gtest.h"
-
-TEST(DateConverterFixture, FirstDayEver){ // 1/1/1 -> 1/3/1
-
-
-    EXPECT_EQ(1,1);
-
-}
--- P1/cmake-build-debug/Google_tests/lib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Projects/Algo-Mag/P1/Google_tests/lib
-# Build directory: C:/Projects/Algo-Mag/P1/cmake-build-debug/Google_tests/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
